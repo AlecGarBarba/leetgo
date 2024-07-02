355bd7430692c2f8fbd03f0ef9812af670a94f9e
Alec García Barba <38474080+AlecGarBarba@users.noreply.github.com>
2024-07-02 17:07:16 -0600

    wordsearch via dfs

---
+++ backtrack/wordsearch/wordsearch.go
@@ -0,0 +1,51 @@
+package wordsearch
+
+/*
+*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+*/
+func exist(board [][]byte, word string) bool {
+	if len(board) == 0 || len(word) > len(board)*len(board[0]) {
+		return false
+	}
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if word[0] == board[i][j] {
+				return dfs(board, word, i, j, 0)
+			}
+		}
+
+	}
+
+	return false
+}
+
+func dfs(board [][]byte, target string, i int, j int, index int) bool {
+	n := len(board)
+	m := len(board[0])
+
+	if index == len(target) {
+		return true
+	}
+
+	// out of bounds, out of luck
+
+	if i < 0 || i >= n {
+		return false
+	}
+
+	if j < 0 || j >= m {
+		return false
+	}
+
+	// if they are not the same, bad luck
+	if board[i][j] != target[index] {
+		return false
+	}
+
+	return (dfs(board, target, i+1, j, index+1) || dfs(board, target, i-1, j, index+1) || dfs(board, target, i, j+1, index+1) || dfs(board, target, i, j-1, index+1))
+
+}
---
+++ backtrack/wordsearch/wordsearch_test.go
@@ -0,0 +1,21 @@
+package wordsearch
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEmptyMatrix(t *testing.T) {
+	assert.False(t, exist([][]byte{}, "AB"))
+
+}
+
+func TestFindFirstWord(t *testing.T) {
+	assert.True(t, exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "A"))
+	assert.False(t, exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "Z"))
+}
+
+func TestExisting(t *testing.T) {
+	assert.True(t, exist([][]byte{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"))
+}
