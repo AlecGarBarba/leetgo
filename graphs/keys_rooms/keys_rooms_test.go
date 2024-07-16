a173f17a67eb657280d776bb3bf24a1440149d7b
Alec García Barba <38474080+AlecGarBarba@users.noreply.github.com>
2024-07-15 22:19:00 -0600

    add more tests for key rooms problem

--- graphs/keys_rooms/keys_rooms_test.go
+++ graphs/keys_rooms/keys_rooms_test.go
@@ -6,6 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEmptyRooms(t *testing.T) {
+
+	rooms := [][]int{}
+	assert.Equal(t, true, canVisitAllRooms(rooms), "Empty rooms should return true")
+
+}
+
 /*
 Example 1:
 
@@ -16,5 +23,12 @@ func TestRoomsWithAllKeys(t *testing.T) {
 
 	rooms := [][]int{{1}, {2}, {3}, {}}
 	assert.Equal(t, true, canVisitAllRooms(rooms), "all keys are available")
+
+}
+
+func TestRoomsWithMissingKey(t *testing.T) {
+
+	rooms := [][]int{{1}, {3}, {2}, {}}
+	assert.Equal(t, false, canVisitAllRooms(rooms), "Rooms have missing key")
 
 }
